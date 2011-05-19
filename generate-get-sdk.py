a3e5e408a3627ac13355db92114bacbd5c5492ad
Alexandros Naskos <alex_naskos@hotmail.com>
2011-05-19 19:37:00 +0300

    Made python script better :D

--- generate-get-sdk.py
+++ generate-get-sdk.py
@@ -3,11 +3,17 @@ from __future__ import with_statement
 import os
 
 IGNORE = [
-    'structs/RangeList',
+    'ooc-yajl/test',
+    'ooc-yajl/simple-test',
+    'ooc-yajl/very-simple-test',
+    'ooc-yajl/yajl/Yajl',
+    'optparse/test',
+    'optparse/optparse/Parser',
+    'optparse/optparse/Option'
 ]
 
 if not 'ROCK_SDK' in os.environ:
-    raise Exception('You have to set $OOC_SDK')
+    raise Exception('You have to set $ROCK_SDK')
 
 sdk = os.environ['ROCK_SDK']
 
@@ -20,7 +26,10 @@ def visit(root):
         package_path = dirname[root_len:].replace(os.pathsep, '/').strip('/')
         for fname in fnames:
             if fname.endswith('.ooc'):
-                modulename = package_path + '/' + fname[:-4]
+                if len(package_path) == 0:
+                    modulename = fname[:-4]
+                else:
+                    modulename = package_path + '/' + fname[:-4]
                 if modulename not in IGNORE:
                     yield modulename
 
